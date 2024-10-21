16372a7d2f2083a2538153fe0116492569a50864
beckhamtoh <beckhamtoh@gmail.com>
2024-10-21 20:06:44 +0800

    Completed SMOTE code

--- src/smote.py
+++ src/smote.py
@@ -4,7 +4,7 @@ from imblearn.over_sampling import SMOTENC
 from sklearn.model_selection import train_test_split
 
 # Load dataset
-data = pd.read_csv('../data/cleaned_trip_data2.csv')
+data = pd.read_csv('../data/cleaned_trip_data.csv') # replace with latest cleaned dataset 
 
 # Drop the date in 'time' column
 data['time'] = pd.to_datetime(data['time'], format='%d/%m/%Y %H:%M') 
@@ -28,25 +28,22 @@ data = data.drop(columns=['yeardate', 'date']) # Dropped 'year' column as all th
 X = data.drop(columns=['major'])  # Features
 y = data['major']  # Target
 
-# Check columns in X
-print("Features columns:", X.columns.tolist())  # Print columns to check for any Timestamp objects
-
 # Categorical cols
 categorical_features =  [0, 1, 2, 5, 6, 7, 8, 9]
 
 # Split the data into training and testing sets 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify = y) # ** add this argument after each major has more than 1 instance (stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify = y)
 
 # SMOTE-NC
 smote_nc = SMOTENC(categorical_features=categorical_features, 
                    categorical_encoder=None, 
                    sampling_strategy='auto', 
                    random_state=0,
-                   k_neighbors=1) # ** change back k_neighbours = 5 with the full data
+                   k_neighbors=5) # ** have to adjust accordingly because the training-test split might have resulted in some majors having lesser than 5 counts
 
 # Apply SMOTE-NC on the training data
-X_resampled, y_resampled = smote_nc.fit_resample(X, y)
-print(y_resampled.value_counts())
+X_resampled, y_resampled = smote_nc.fit_resample(X_train, y_train)
+
 # Save resampled data
 resampled_data = pd.DataFrame(X_resampled, columns=X.columns)  # Create dataframe for resampled features
 resampled_data['major'] = y_resampled  # add back major column to the resampled data
