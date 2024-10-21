8b9cc28f168193c5737135e88f3233c39665a32a
beckhamtoh <beckhamtoh@gmail.com>
2024-10-21 20:06:28 +0800

    Working SMOTE code

--- src/smote.py
+++ src/smote.py
@@ -4,36 +4,50 @@ from imblearn.over_sampling import SMOTENC
 from sklearn.model_selection import train_test_split
 
 # Load dataset
-data = pd.read_csv('cleaned_trip_data.csv')
+data = pd.read_csv('../data/cleaned_trip_data2.csv')
 
 # Drop the date in 'time' column
-data['time'] = pd.to_datetime(data['time'], format='%Y-%m-%d %H:%M:%S').dt.strftime('%H:%M')
+data['time'] = pd.to_datetime(data['time'], format='%d/%m/%Y %H:%M') 
+data['hour'] = data['time'].dt.hour
+data['minute'] = data['time'].dt.minute
+data.drop(columns=['time'], inplace=True)
 
+data['year'] = data['year'].str.replace('Year ', '').astype(int) # Remove 'Year' from 'year' column, can do in data cleaning?
 
 # Convert 'date' column to datetime format
-data['date'] = pd.to_datetime(data['date'])
+data['date'] = pd.to_datetime(data['date'], format='%d/%m/%Y')
 
-# encode date
-data['year'] = data['date'].dt.year
+# Encode date
+data['yeardate'] = data['date'].dt.year
 data['month'] = data['date'].dt.month
 data['day_of_week'] = data['date'].dt.dayofweek
+
+data = data.drop(columns=['yeardate', 'date']) # Dropped 'year' column as all the years should be 2024 and considered a redundant feature
 
 
 X = data.drop(columns=['major'])  # Features
 y = data['major']  # Target
 
+# Check columns in X
+print("Features columns:", X.columns.tolist())  # Print columns to check for any Timestamp objects
+
 # Categorical cols
-categorical_features =  [0, 1, 2, 6, 7, 8, 9, 11]
+categorical_features =  [0, 1, 2, 5, 6, 7, 8, 9]
 
 # Split the data into training and testing sets 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify = y) # ** add this argument after each major has more than 1 instance (stratify=y)
 
 # SMOTE-NC
 smote_nc = SMOTENC(categorical_features=categorical_features, 
                    categorical_encoder=None, 
                    sampling_strategy='auto', 
-                   random_state=0, 
-                   k_neighbors=5)
+                   random_state=0,
+                   k_neighbors=1) # ** change back k_neighbours = 5 with the full data
 
 # Apply SMOTE-NC on the training data
-X_resampled, y_resampled = smote_nc.fit_resample(X_train, y_train)
+X_resampled, y_resampled = smote_nc.fit_resample(X, y)
+print(y_resampled.value_counts())
+# Save resampled data
+resampled_data = pd.DataFrame(X_resampled, columns=X.columns)  # Create dataframe for resampled features
+resampled_data['major'] = y_resampled  # add back major column to the resampled data
+resampled_data.to_csv('../data/resampled_trip_data.csv', index=False)
