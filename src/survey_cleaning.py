1bbf3d470af06ffc772f1143c7aab586c8078a98
chiabingxuan <bingxuan2039@gmail.com>
2024-10-23 13:24:02 +0800

    Added some preliminary data exploration and removal of outliers

--- src/survey_cleaning.py
+++ src/survey_cleaning.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 import datetime
-import re
 import major_mappings
 
 def reshape(survey_data):
@@ -16,11 +16,13 @@ def reshape(survey_data):
 
 def rename_columns(data, new_col_names):
     data.columns = new_col_names
+    print(f"Renamed columns: New column names are {new_col_names}")
 
 
 def trim_whitespace(data, col_names):
     for col_name in col_names:
         data[col_name] = data[col_name].str.strip()
+    print(f"Completed trimming of whitespace for {col_names}")
 
 
 def choose_first_int_only(entry):
@@ -37,8 +39,10 @@ def choose_first_int_only(entry):
 def convert_columns_to_int(data, col_names):
     for col_name in col_names:
         data[col_name] = data[col_name].astype(str)   # convert column to str
-        data[col_name] = data[col_name].map(choose_first_int_only)    # choose the first number in the entry (removes all other characters that follow)
-        data[col_name] = data[col_name].astype(int)   # convert column to int
+        modified_col = data[col_name].map(choose_first_int_only)    # choose the first number in the entry (removes all other characters that follow)
+        num_entries_changed = (data[col_name] != modified_col).sum()    # compare original column (str data type) with the modified column and count the differences
+        data[col_name] = modified_col.astype(int)   # convert modified column to int and use it to replace the original column in data
+        print(f"Converted columns to int data type: Number of entries modified for {col_name} is {num_entries_changed}")
 
 
 def format_dates(data, col_names):  # col_names: names of all columns with dates
@@ -59,7 +63,10 @@ def correct_indiv_time(trip_time):
 def format_and_correct_times(data, col_names):  # col_names: names of all columns with times
     for col_name in col_names:
         data[col_name] = pd.to_datetime(data[col_name], format="%I:%M:%S %p")   # convert to datetime
-        data[col_name] = data[col_name].map(correct_indiv_time) # correct any invalid times
+        modified_col = data[col_name].map(correct_indiv_time) # correct any invalid times
+        num_entries_changed = (data[col_name] != modified_col).sum()    # compare original column (datetime data type) with the modified column and count the differences
+        data[col_name] = modified_col
+        print(f"Formatted and corrected invalid times: Number of entries modified for {col_name} is {num_entries_changed}")
 
 def correct_individual_major(major):
     # Step 1: Dictionary to get the mappings
@@ -70,26 +77,15 @@ def correct_individual_major(major):
 
     # Step 3: Replace using the mapping dictionary, if applicable
     cleaned_major = MAPPINGS.get(lower_major, lower_major)  # Use the original field if not found in mapping
-    
-    """
-    Step 4: Apply known corrections
-    for wrong, right in mappings.items():
-        major = re.sub(wrong, right, major)
-
-    major = re.sub(r'([a-z])([A-Z])', r'\1 \2', major)
-    
-    # Step 5: Remove special characters
-    major = re.sub(r'[^a-z\s]', '', major)
-    
-    # Step 6: Replace multiple spaces with a single space
-    major = re.sub(r'\s+', ' ', major).strip()
-    """
 
     return cleaned_major
 
 
 def clean_majors(data, col_name):   # col_name: name of columns with major
-    data[col_name] = data[col_name].map(correct_individual_major)
+    modified_col = data[col_name].map(correct_individual_major)
+    num_entries_changed = (data[col_name] != modified_col).sum()    # compare original column with the modified column and count the differences
+    data[col_name] = modified_col
+    print(f"Standardised majors: Number of entries modified for {col_name} is {num_entries_changed}")
 
 
 def check_start_end_has_bus_num(start, end, bus_num):
@@ -112,11 +108,16 @@ def check_start_end_has_bus_num(start, end, bus_num):
 
 def remove_invalid_trips(data, start, end, bus_num):
     # Starting and ending bus stops cannot be the same
-    data.drop(data[data[start] == data[end]].index, inplace=True)
+    are_start_end_the_same = data[start] == data[end]
+    num_trips_with_start_end_the_same = are_start_end_the_same.sum()    # count number of trips with start == end
+    data.drop(data[are_start_end_the_same].index, inplace=True)
+    print(f"Removed invalid bus trips: Number of rows where starting and ending bus stops are the same is {num_trips_with_start_end_the_same}")
 
     # bus_num must be serviced in both starting and ending bus stops
-    do_trips_have_bus_num_in_start_end = data.apply(lambda row: not check_start_end_has_bus_num(row[start], row[end], row[bus_num]), axis=1)
-    data.drop(data[do_trips_have_bus_num_in_start_end].index, inplace=True)
+    do_trips_not_have_bus_num_in_start_end = data.apply(lambda row: not check_start_end_has_bus_num(row[start], row[end], row[bus_num]), axis=1)
+    num_trips_not_have_bus_num_in_start_end = do_trips_not_have_bus_num_in_start_end.sum()      # count number of trips where bus_num is not serviced in both start and end
+    data.drop(data[do_trips_not_have_bus_num_in_start_end].index, inplace=True)
+    print(f"Removed invalid bus trips: Number of rows where bus number is not serviced in both starting and ending bus stops is {num_trips_not_have_bus_num_in_start_end}")
 
 
 def clean_feedback(data, col_name):
@@ -124,6 +125,8 @@ def clean_feedback(data, col_name):
 
 
 def clean_trip_data(trip_data):
+    print("Cleaning trip data...")
+
     # Rename columns
     rename_columns(trip_data, ["year", "major", "on_campus", "main_reason_for_taking_isb", "trips_per_day", "duration_per_day", "date", "has_exam", "start", "end", "bus_num", "time", "weather", "num_people_at_bus_stop", "waiting_time", "waiting_time_satisfaction", "crowdedness", "crowdedness_satisfaction", "comfort", "safety", "overall_satisfaction"])
 
@@ -143,8 +146,12 @@ def clean_trip_data(trip_data):
     # Remove trips that don't make sense
     remove_invalid_trips(trip_data, "start", "end", "bus_num")
 
+    print("Trip data cleaned!\n")
+
 
 def clean_other_feedback_data(other_feedback_data):
+    print("Cleaning other feedback data...")
+
     # Rename columns
     rename_columns(other_feedback_data, ["year", "major", "on_campus", "main_reason_for_taking_isb", "trips_per_day", "duration_per_day", "date", "has_exam", "feedback"])
 
@@ -163,11 +170,68 @@ def clean_other_feedback_data(other_feedback_data):
     # Clean feedback data
     clean_feedback(other_feedback_data, "feedback")
 
+    print("Other feedback data cleaned!\n")
+
+
+def visualise_data(data, categorical_vars, continuous_vars):
+    for var_index in categorical_vars:
+        var_name = data.columns[var_index]
+        plt.figure(figsize=(12,6))
+        data.iloc[:, var_index].value_counts().plot(kind="barh")
+        plt.title(f"Bar chart for {var_name}")
+        plt.xlabel("Count")
+        plt.ylabel(var_name)
+        plt.tight_layout()
+        plt.show()
+    for var_index in continuous_vars:
+        var_name = data.columns[var_index]
+        data.iloc[:, var_index].plot(kind="hist")
+        plt.title(f"Histogram for {data.columns[var_index]}")
+        plt.xlabel(var_name)
+        plt.ylabel("Frequency")
+        plt.tight_layout()
+        plt.show()
+
+
+def remove_outliers(data, year_of_study, num_people_at_bus_stop=None):
+    if num_people_at_bus_stop is not None:  # means that data provided is trip_data
+        print("Removing outliers for trip data...")
+    else:
+        print("Removing outliers for other feedback data...")
+
+    # Remove rows that correspond to Year 5 or Masters / PhD
+    is_year_5_and_above = data[year_of_study].map(lambda year: year in ["Year 5", "Masters / PhD"])
+    num_is_year_5_and_above = is_year_5_and_above.sum()    # count number of rows with year being Year 5 and above
+    data.drop(data[is_year_5_and_above].index, inplace=True)
+    print(f"Removed outliers for {year_of_study}: Number of rows that are either 'Year 5' or 'Masters / PhD' is {num_is_year_5_and_above}")
+
+    # Remove rows that have unrealistically large num_people_at_bus_stop
+    if num_people_at_bus_stop is not None:  # means that data provided is trip_data
+        has_large_num_people_at_bus_stop = data[num_people_at_bus_stop] >= 80   # set upper limit at 80
+        num_has_large_num_people_at_bus_stop = has_large_num_people_at_bus_stop.sum()   # count number of rows with unrealistically large num_people_at_bus_stop
+        data.drop(data[has_large_num_people_at_bus_stop].index, inplace=True)
+        print(f"Removed outliers for {num_people_at_bus_stop}: Number of rows that have unrealistically large {num_people_at_bus_stop} is {num_has_large_num_people_at_bus_stop}")
+    
+    print("Outliers removed!\n")
+
 
 if __name__ == "__main__":
+    # Read in data
     survey_data = pd.DataFrame(pd.read_csv(os.path.join(os.path.dirname(__file__), "../data/survey.csv"), keep_default_na=False))
+
+    # Initial reshaping and cleaning of data
     trip_data, other_feedback_data = reshape(survey_data)
     clean_trip_data(trip_data)
     clean_other_feedback_data(other_feedback_data)
+
+    # Conduct preliminary data exploration
+    # visualise_data(trip_data, categorical_vars=[0, 1, 2, 3, 7, 8, 9, 10, 12], continuous_vars=[4, 5, 13, 14, 15, 16, 17, 18, 19, 20])    # omit date and time column
+    # visualise_data(other_feedback_data, categorical_vars=[0, 1, 2, 3, 7], continuous_vars=[4, 5])    # omit date column
+
+    # Remove outliers
+    remove_outliers(trip_data, "year", num_people_at_bus_stop="num_people_at_bus_stop")
+    remove_outliers(other_feedback_data, "year")
+
+    # Save cleaned data frames
     trip_data.to_csv(os.path.join(os.path.dirname(__file__), "../data/cleaned_trip_data.csv"), index=False)
     other_feedback_data.to_csv(os.path.join(os.path.dirname(__file__), "../data/cleaned_other_feedback_data.csv"), index=False)
