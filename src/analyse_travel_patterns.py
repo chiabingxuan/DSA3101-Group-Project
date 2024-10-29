c5523057b680bc2f11e11e34a3ae515357191b6b
chiabingxuan <bingxuan2039@gmail.com>
2024-10-30 00:14:17 +0800

    Included the creation of overall and timelapsed heat maps in analyse_travel_patterns.py

--- src/analyse_travel_patterns.py
+++ src/analyse_travel_patterns.py
@@ -6,14 +6,32 @@ from collections import defaultdict
 import os
 import pandas as pd
 import numpy as np
+import datetime
 import config
 
-def extract_time(data, time):
+
+def convert_to_one_hour_range(dt): # dt: datetime object from "time" column
+    # Get start and end hours in str
+    start_hour = str(dt.hour)
+    end_hour = str((dt + datetime.timedelta(hours=1)).hour)
+
+    # Pad hours with leading zeroes where necessary
+    if len(start_hour) == 1:
+        start_hour = "0" + start_hour
+    if len(end_hour) == 1:
+        end_hour = "0" + end_hour
+    
+    # Make range
+    range_str = f"{start_hour}:00:00 - {end_hour}:00:00"
+    return range_str
+
+
+def extract_hour(data, time):
     # Convert to datetime
     data[time] = pd.to_datetime(data[time], format="%Y-%m-%d %H:%M:%S")
 
-    # Extract time
-    data[time] = data[time].map(lambda dt: dt.time())
+    # For each datetime, convert to a 1 hour range (str)
+    data[time] = data[time].map(convert_to_one_hour_range)
 
 
 def add_coordinates_to_data(data, start, end):      # start and end both in the form (lat, long)
@@ -33,27 +51,37 @@ def generate_proportions_concentrated_at_0_and_1(num_points):
     return proportions
 
 
-def get_heat_data(data, num_points_sampled_per_line):
-    # Get gdf
-    data["line_geometry"] = data.apply(lambda row: LineString([(row["start_long"], row["start_lat"]), (row["end_long"], row["end_lat"])]), axis=1)
-    gdf = gpd.GeoDataFrame(data, geometry="line_geometry")
+def get_first_hour_from_time_range(time_range):     # time_range: string corresponding to 1 hour range
+    first_hour_str = time_range.split()[0]
+    first_hour = datetime.datetime.strptime(first_hour_str, "%H:%M:%S")
+    return first_hour
 
+
+def get_heat_data_for_overall_heat_map(data):
+    # Get heat data
+    heat_data = list()
+    for _, row in data.iterrows():
+        start_long, start_lat, end_long, end_lat = row["start_long"], row["start_lat"], row["end_long"], row["end_lat"]
+
+        # Add the start and end coordinates to the heat data
+        heat_data.extend([(start_lat, start_long), (end_lat, end_long)])  # (lat, long) format for folium
+
+    return heat_data
+
+
+def get_heat_data_and_time_indices_for_timelapse(data):
     # Initialise storage for points grouped by time
     heat_data_dict = defaultdict(list)
 
     # Get heat data and time indices
-    for _, row in gdf.iterrows():
-        time, line = row["time"], row["line_geometry"]
-        proportions_of_length = generate_proportions_concentrated_at_0_and_1(num_points_sampled_per_line)
-
-        # For each proportion, which represents how far along the line with must go, the corresponding point is sampled. Proportions are concentrated at 0 and 1, since buses are more likely to accumulate at the bus stops themselves
-        points = [line.interpolate(proportion * line.length) for proportion in proportions_of_length]
-
-        # Add the sampled points to the heat data
-        heat_data_dict[time].extend([[point.y, point.x] for point in points]) # (lat, long) format for folium
+    for _, row in data.iterrows():
+        time, start_long, start_lat, end_long, end_lat = row["time"], row["start_long"], row["start_lat"], row["end_long"], row["end_lat"]
+        
+        # Add the start and end coordinates to the heat data
+        heat_data_dict[time].extend([[start_lat, start_long], [end_lat, end_long]]) # (lat, long) format for folium
     
     # Convert dictionary to sorted lists by time
-    time_indices = sorted(heat_data_dict.keys())    # time_indices: all times in ascending order
+    time_indices = sorted(heat_data_dict.keys(), key=get_first_hour_from_time_range)    # time_indices: all times in ascending order
     heat_data = [heat_data_dict[time_index] for time_index in time_indices]     # heat_data: list of coordinates (coordinates represented as a list of length 2) for each time_index
     return heat_data, time_indices
 
@@ -62,16 +90,21 @@ if __name__ == "__main__":
     # Read trip_data
     trip_data = pd.read_csv(os.path.join(os.path.dirname(__file__), "../data/train_trip_data_after_sdv.csv"), keep_default_na=False)
 
-    # Extract time only from "time" column of trip_data
-    extract_time(trip_data, "time")
+    # Extract hour only from "time" column of trip_data
+    extract_hour(trip_data, "time")
 
     # To the trip_data, we add coordinates of starting and ending bus stops
     add_coordinates_to_data(trip_data, "start", "end")
-    heat_data, time_indices = get_heat_data(trip_data, num_points_sampled_per_line=50)
+    
+    heat_data_for_overall_heat_map = get_heat_data_for_overall_heat_map(trip_data)
+    heat_data_for_timelapse, time_indices = get_heat_data_and_time_indices_for_timelapse(trip_data)
+
+    # Generate NUS map and apply the overall heat map to it
+    map = folium.Map(location=config.NUS_COORDINATES, zoom_start=15)
+    plugins.HeatMap(heat_data_for_overall_heat_map, radius=30).add_to(map)
+    map.save(os.path.join(os.path.dirname(__file__), "../visualisations/nus_heat_map.html"))
 
     # Generate NUS map and apply the heat map timelapse to it
-    map = folium.Map(location=config.NUS_COORDINATES, zoom_start=16)
-    plugins.HeatMapWithTime(heat_data, index=[str(time_index) for time_index in time_indices], radius=8, auto_play=True, display_index=True, max_opacity=0.8).add_to(map)
-
-    # Save map
+    map = folium.Map(location=config.NUS_COORDINATES, zoom_start=15)
+    plugins.HeatMapWithTime(heat_data_for_timelapse, index=[str(time_index) for time_index in time_indices], radius=30, auto_play=True, display_index=True).add_to(map)
     map.save(os.path.join(os.path.dirname(__file__), "../visualisations/nus_heat_map_timelapse.html"))
