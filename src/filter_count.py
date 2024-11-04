3f5b1025746964f9306455adeeb56f9fc13d6b0d
beckhamtoh <beckhamtoh@gmail.com>
2024-11-05 00:36:36 +0800

    Fixed floor to ceiling in filter_count.py

--- src/filter_count.py
+++ src/filter_count.py
@@ -5,16 +5,17 @@ def filter_route_counts(trip_data, output_file_name, min_count):
     data = trip_data
 
     data['time'] = pd.to_datetime(data['time'])
-    data['time_floored'] = data['time'].dt.floor('10T')
+    
+    data['time_ceiling'] = data['time'].dt.ceil('10T')
 
     data['route'] = data['start'] + ' - ' + data['end']
 
-    route_time_counts = data.groupby(['route', 'bus_num', 'time_floored']).size().reset_index(name='count')
+    route_time_counts = data.groupby(['route', 'bus_num', 'time_ceiling']).size().reset_index(name='count')
 
     filtered_counts = route_time_counts[route_time_counts['count'] > min_count]
 
     filtered_counts[['start', 'end']] = filtered_counts['route'].str.split(' - ', expand=True)
 
-    filtered_counts = filtered_counts[['start', 'end', 'bus_num', 'time_floored', 'count']]
+    filtered_counts = filtered_counts[['start', 'end', 'bus_num', 'time_ceiling', 'count']]
 
     filtered_counts.to_csv(os.path.join(os.path.dirname(__file__), f"../data/{output_file_name}.csv"), index=False)
